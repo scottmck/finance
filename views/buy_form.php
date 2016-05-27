c86d1258ae492698eaf5cd657895c679af25299e
scottmck <arthurstreeb@hotmail.com>
2016-05-27 13:27:37 +1200

    Create buy_form.php
    Working but need to css and/or bootstrap

---
+++ views/buy_form.php
@@ -0,0 +1,21 @@
+<div>
+
+        <form action="buy.php" method="post">
+            <fieldset>
+                <div class="form-group">
+                    <input autocomplete="off" autofocus class="form-control" name="buy_stock" placeholder="Stock symbol to buy" type="text"/>
+                </div>
+                <div class="form-group">
+                    <input autocomplete="off" autofocus class="form-control" name="buy_amount" placeholder="Amount to buy" type="number"/>
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-default" type="submit">
+                        <span aria-hidden="true" class="glyphicon glyphicon-log-in"></span>
+                        Submit
+                    </button>
+                </div>
+            </fieldset>
+        </form>
+    <div>
+        or <a href="index.php">go back to portfolio</a>
+    </div>
