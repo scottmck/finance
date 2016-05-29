a9a29abf28a5dfabc0948d504d6c1e40bfae8df3
scottmck <arthurstreeb@hotmail.com>
2016-05-30 11:00:42 +1200

    tidied up sell_display.php
    Added div id tags and formatted for readability

--- views/sell_display.php
+++ views/sell_display.php
@@ -20,33 +20,34 @@
     
 <!--below is the display for portfolio -->   
     <div>
-    <div id="message">
-        <?php
-            print("Welcome back ". $cash[0]["username"] .". Your balance is " . number_format($cash[0]["cash"], 2));?>
+        <div id="message">
+            <?php
+                print("Hi ". $cash[0]["username"] .". Your balance is " . number_format($cash[0]["cash"], 2));?>
+        </div>
+        <div id="tableid">    
+            <table class="center"; border="2">
+                <thead>
+                    <th>Stock Symbol</th>
+                    <th>Name</th>
+                    <th>Shares</th>
+                    <th>Price</th>
+                    <th>Total Shares Cost</th>
+                </thead>
+            
+                <tbody>
+                    <?php foreach ($positions as $position): ?>
+                    <tr>    
+                        <td><?= $position["symbol"]?></td>
+                        <td><?= $position["name"]?></td>
+                        <td><?= $position["shares"]?></td>
+                        <td>$<?= number_format($position["price"], 2)?></td>
+                        <td>$<?= number_format($position["total"], 2)?></td>
+                    </tr>
+                    <?php endforeach ?>    
+                </tbody>
+            </table>
+        </div>
     </div>
-    <table class="center"; border="2">
-    <thead>
-        <th>Stock Symbol</th>
-        <th>Name</th>
-        <th>Shares</th>
-        <th>Price</th>
-        <th>Total Shares Cost</th>
-    </thead>
-    
-    <tbody>
-    <?php foreach ($positions as $position): ?>
-        <tr>    
-            <td><?= $position["symbol"]?></td>
-            <td><?= $position["name"]?></td>
-            <td><?= $position["shares"]?></td>
-            <td><?= number_format($position["price"], 2)?></td>
-            <td><?= number_format($position["total"], 2)?></td>
-        </tr>
-    <?php endforeach ?>    
-    </tbody>
-</table>
-</div>
             
 </div>
-    
     
