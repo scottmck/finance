ada08a4bb7ea0cf34a4669c33401481482387de2
scottmck <arthurstreeb@hotmail.com>
2016-05-29 16:05:16 +1200

    taken out total cash and greeting

--- views/history_display.php
+++ views/history_display.php
@@ -1,27 +1,22 @@
 <div>
-    <div id="message">
-        <?php
-            print("Welcome back ". $cash[0]["username"] .". Your balance is " . number_format($cash[0]["cash"], 2));?>
-    </div>
+
     <table class="center"; border="2">
     <thead>
         <th>Buy/Sell</th>
         <th>Symbol</th>
+        <th>Share Number</th>
         <th>Time</th>
-        <th>Amount</th>
         <th>Price</th>
-        <th>Total Price</th>
     </thead>
     
     <tbody>
     <?php foreach ($positions as $position): ?>
         <tr>    
-            <td><?= $position["buy/sell"]?></td>
+            <td><?= $position["buy_or_sell"]?></td>
             <td><?= $position["symbol"]?></td>
+            <td><?= $position["share_number"]?></td>
             <td><?= $position["time"]?></td>
-            <td><?= $position["amount"]?></td>
             <td><?= $position["price"]?></td>
-            <td><?= $position["total_price"]?></td>
         </tr>
     <?php endforeach ?>    
     </tbody>
