be9ac25a3e39ae1220c620ff4477c0c8dc010dc4
scottmck <arthurstreeb@hotmail.com>
2016-05-30 11:03:18 +1200

    tidied stock_display.php
    Added div id and removed quote header to reduce page clutter

--- views/stock_display.php
+++ views/stock_display.php
@@ -1,15 +1,16 @@
-<h3>Quote display</h3>
-    
-<table class="center"; border="2">
-    <tr>
-        <td>Stock Symbol</td>
-        <td>Stock Name</td>
-        <td>Stock Price</td>
-    </tr>
-    <tr>
-        <td><?=print($stock["symbol"])?></td>
-        <td><?=print($stock["name"])?></td>
-        <td><?=print($stock["price"])?></td>
-    </tr>
-    
-</table>
+
+<div id="tableid">
+    <table class="center"; border="2">
+        <tr>
+            <td>Stock Symbol</td>
+            <td>Stock Name</td>
+            <td>Stock Price</td>
+        </tr>
+        <tr>
+            <td><?=print($stock["symbol"])?></td>
+            <td><?=print($stock["name"])?></td>
+            <td><?=print($stock["price"])?></td>
+        </tr>
+        
+    </table>
+</div>    
