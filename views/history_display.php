d1c6ac20e8a900b59f4e4d2b22d50f74911ff522
scottmck <arthurstreeb@hotmail.com>
2016-05-30 11:08:17 +1200

    tidied history_display
    Cleaned and formatted

--- views/history_display.php
+++ views/history_display.php
@@ -1,24 +1,24 @@
-<div>
+<div id="tableid">
 
     <table class="center"; border="2">
-    <thead>
-        <th>Buy/Sell</th>
-        <th>Symbol</th>
-        <th>Share Number</th>
-        <th>Time</th>
-        <th>Price</th>
-    </thead>
-    
-    <tbody>
-    <?php foreach ($positions as $position): ?>
-        <tr>    
-            <td><?= $position["buy_or_sell"]?></td>
-            <td><?= $position["symbol"]?></td>
-            <td><?= $position["share_number"]?></td>
-            <td><?= $position["time"]?></td>
-            <td><?= $position["price"]?></td>
-        </tr>
-    <?php endforeach ?>    
-    </tbody>
-</table>
+        <thead>
+            <th>Buy/Sell</th>
+            <th>Symbol</th>
+            <th>Share Number</th>
+            <th>Time</th>
+            <th>Price</th>
+        </thead>
+        
+        <tbody>
+        <?php foreach ($positions as $position): ?>
+            <tr>    
+                <td><?= $position["buy_or_sell"]?></td>
+                <td><?= $position["symbol"]?></td>
+                <td><?= $position["share_number"]?></td>
+                <td><?= $position["time"]?></td>
+                <td><?= $position["price"]?></td>
+            </tr>
+            <?php endforeach ?>    
+        </tbody>
+    </table>
 </div>
