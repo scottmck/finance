b5669181e2ec1757e895d4c2d143b937106e3399
scottmck <arthurstreeb@hotmail.com>
2016-05-30 14:09:57 +1200

    Update quote.php
    Tidied  and  formatted for readability

--- public/quote.php
+++ public/quote.php
@@ -1,38 +1,31 @@
-
-
 <?php
 
 require("../includes/config.php");
 
-
-//render("quote_form.php", ["title" => "Quote"]);
-if ($_SERVER["REQUEST_METHOD"] == "GET")
+    //render("quote_form.php", ["title" => "Quote"]);
+    if ($_SERVER["REQUEST_METHOD"] == "GET")
     {
         //render form
-       render("quote_form.php", ["title" => "Quote"]);
+        render("quote_form.php", ["title" => "Quote"]);
     } 
-else if ($_SERVER["REQUEST_METHOD"] == "POST")
+    else if ($_SERVER["REQUEST_METHOD"] == "POST")
     {
-        $stock = lookup($_POST["symbol"]);
-        // validate submission
-       if ($stock === false)
-       {
-           //apologize("sorry. Please enter another stock symbol");
-           render("quote_form.php", ["title" => "Quote"]);
-       }
-       else
-       {
-           render("stock_display.php", ["stock" => $stock]);
-       }
-
-    }
-else
-{
-    apologize("sorry something went wrong");
-    render("quote_form.php", ["title" => "Quote"]);
-}
-    
+        $stock = lookup(strtoupper($_POST["symbol"]));
         
-
-
+        // validate submission
+        if ($stock === false)
+        {
+            //apologize("sorry. Please enter another stock symbol");
+            render("quote_form.php", ["title" => "Quote"]);
+        }
+        else
+        {
+            render("stock_display.php", ["stock" => $stock]);
+        }
+    }
+    else
+    {
+        apologize("sorry something went wrong");
+        render("quote_form.php", ["title" => "Quote"]);
+    }
 ?>
