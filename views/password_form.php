fd0e0d15ba7d05710a32f9a52f1cf79321532ec6
scottmck <arthurstreeb@hotmail.com>
2016-05-30 12:07:50 +1200

    Create password_form.php

---
+++ views/password_form.php
@@ -0,0 +1,19 @@
+<form action="password.php" method="post">
+    <fieldset>
+        <div class="form-group">
+            <input autocomplete="off" autofocus class="form-control" name="password" placeholder="Current password" type="password"/>
+        </div>
+        <div class="form-group">
+            <input class="form-control" name="newpassword" placeholder="New Password" type="password"/>
+        </div>
+        <div class="form-group">
+            <input class="form-control" name="newpassword2" placeholder="Retype New Password" type="password"/>
+        </div>
+        <div class="form-group">
+            <button class="btn btn-default" type="submit">
+                <span aria-hidden="true" class="glyphicon glyphicon-log-in"></span>
+                Submit
+            </button>
+        </div>
+    </fieldset>
+</form>
